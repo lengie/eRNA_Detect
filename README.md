5a1980bafe0b068b0d59a4d8552f4e692f7024d7
lengie <cookiestork@gmail.com>
2017-04-26 14:53:19 -0700

    Edited readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# eRNA_Detect
+# eRNA Detect
+
+This program takes RNA-seq data and detects bidirectionally read transcription sites in non coding regions.
+
+Work in progress.
+
+## Contact
+
+engie at usc dot edu 
---
+++ detectEnhancers.r
@@ -0,0 +1,44 @@
+### detectEnhancers.R
+###
+### Purpose: Take RNA-seq data and output locations and FPKMs of regions with bidirectional reads within a given region 
+###
+###
+### Written by Liana Engie
+### Last updated: April 2017
+###
+### detectEnhancers(chromosome, input_start, input_end,strand)
+### Input: string chromosome number, 
+### Output:
+
+ranges <- removeExons(chromosome,input_start,input_end,strand,txdb){
+	start <- c()
+	end <- c()
+	
+
+}
+
+removeZeroCounts(chromosome, ranges, strand="+", sense_counts){
+
+	if(strand!="-"||strand!="+"){
+		print('Strand should be "-" or "+"')
+		break 
+	}
+
+	j <- 0
+	newranges <- IRanges()
+	i <- length(min_counts)
+	if(sense_counts[i]==0){
+		newranges <- append(newranges,ranges[i])
+		j <- j+1
+	}if(j==0){break}
+
+#	if(strand=="-"){
+#		anti <- "+"
+#	}else{anti <- "-"}
+
+	int <- GRanges(seqnames=chromosome,ranges=newranges,strand=anti)
+	counts <- summarizeOverlaps(features=int,reads=hetsread1,singleEnd=FALSE,fragments=FALSE,inter.feature=FALSE)
+	anti_counts <- assay(counts)
+
+}
+
