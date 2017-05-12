f67797562f3d293d78711cf26afd94288382dc30
lengie <cookiestork@gmail.com>
2017-05-11 18:07:26 -0700

    adding FPKM calc to test

--- detectEnhancers.r
+++ detectEnhancers.r
@@ -22,7 +22,7 @@ clusters <- findReadRegions(chromosome,input_start,input_end,strand,bed){
 		break 
 	}
 	
-	interval <- IRanges(input_start,input_end)
+	interval <- GRanges(seqnames=chromosome,range=IRanges(input_start,input_end),strand=strand)
 	list <- subsetByOverlaps(bed,interval) #GRanges obj
 	if(length(ranges(list))==0){
 		print('No clusters of reads in range.')
@@ -64,6 +64,21 @@ TPM <- TPMCalc(clusters, reads){
 	}
 	scaling <- sum(rpk)/1000000 #except this should be for the whole sample
 	TPM <- rpk/scaling 
+	
+}
+
+FPKM <- FPKMCalc(clusters,reads,scaling_factor){
+	counts <- summarizeOverlaps(features=clusters,reads=reads,singleEnd=FALSE,fragments=FALSE,inter.feature=FALSE)
+	sense_counts <- assay(counts)
+	#just read strand from GRanges obj
+	opp_strand <- reverseStrand(clusters)
+	
+	counts <- summarizeOverlaps(features=opp_strand,reads=reads,singleEnd=FALSE,fragments=FALSE,inter.feature=FALSE)
+	anti_counts <- assay(counts)
+	
+	
+	FPKM <- data.frame()
+	colnames(FPKM) <- c("start","width","sense","antisense")
 }
 
 hets1 <- "/auto/cmb-00/rr/engie/RNA/Aligned.sortedByCoord.out.bam" 
@@ -80,3 +95,8 @@ bed1R <- GRanges(seqnames=bed1$V1,ranges=IRanges(start=bed1$V2, end=bed1$V3),str
 clusters <- findReadRegions("chr1",39693870,39800883,"-",bed1R)
 MAML_TPM <- TPMCalc(clusters,hetsread1)
 MAML_TPM2 <- TPMCalc(clusters,hetsread2)
+
+bach2 <- findReadRegions("chr20",24073750,24223950, "+",bed1R)
+bach2FPKM <- FPKMCalc(bach2,hetsread1,hets1frag)
+bach2FPKMcheck <- FPKMCalc(bach2,hetsread3,hets2frag)
+
