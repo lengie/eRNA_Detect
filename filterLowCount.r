8034532536b873e14f15bf5f05b662f3ede1a7ff
lengie <cookiestork@gmail.com>
2021-10-18 23:02:22 -0700

    fixed width GRanges error

--- filterLowCount.r
+++ filterLowCount.r
@@ -7,6 +7,7 @@ library(data.table)
 library(GenomicRanges)
 
 
+
 lowcount <- function(bed,bam){
     feat <- GRanges(bed)
 
@@ -14,7 +15,7 @@ lowcount <- function(bed,bam){
     seqlevelsStyle(feat) <- "ensembl"
 
     strand(feat) <- '+'
-    counts <- summarizeOverlaps(features=feat,reads=bam,ignore.strand=FALSE)      
+    counts <- summarizeOverlaps(features=feat,reads=bam,ignore.strand=FALSE) 
     genecounts <- assay(counts)
     strand(feat) <- '-'
     counts <- summarizeOverlaps(features=feat,reads=bam,ignore.strand=FALSE)      
@@ -23,7 +24,7 @@ lowcount <- function(bed,bam){
     print(paste("Number of regions: ",dim(genecounts),sep="")) #this is just the number of bidirectional regions
     print(paste("Regions with with 0 reads on plus strand: ",length(which(genecounts[,1]==0)),sep=""))
     print(paste("Regions with with 0 reads on minus strand: ",length(which(genecounts[,2]==0)),sep=""))
-    widthplot <- data.frame(rpb=c(genecounts[,1]/feat$width,genecounts[,2]/feat$width),
+    widthplot <- data.frame(rpb=c(genecounts[,1]/width(feat) , genecounts[,2]/width(feat),
                     strand=rep(c('+','-'),each=nrow(genecounts)))
     justreadsplot <- data.frame(rpb=c(genecounts[,1],genecounts[,2]),
                     strand=rep(c('+','-'),each=nrow(genecounts)))
